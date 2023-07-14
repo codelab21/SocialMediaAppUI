76ba8b8302bb6697a223da45756315653a4b9325
codelab21 <codelab21@outlook.com>
2023-07-14 16:15:27 +0300

    Initial Commit

---
+++ SocialMediaTutorial/SocialMediaTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialMediaTutorialApp.swift
+//  SocialMediaTutorial
+//
+//  Created by Eymen on 14.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct SocialMediaTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
